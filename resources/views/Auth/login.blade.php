929d6880aca2ef81ebfbfe534d24afe069a81a37
Haqmal <haqmal.100.zzz@gmail.com>
2026-09-23 10:26:04 +0430

    login page

---
+++ resources/views/Auth/login.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+    <h1>This is login page</h1>
+
+    <form>
+        <div>
+            <label>Email:</label>
+            <input type="email" name="email">
+        </div>
+
+        <br>
+
+        <div>
+            <label>Password:</label>
+            <input type="password" name="password">
+        </div>
+
+        <br>
+
+        <button type="submit">Login</button>
+    </form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+
+
+});
+
+    Route::get('/Auth/login', function () {
+    return view('Auth.login');
 });
